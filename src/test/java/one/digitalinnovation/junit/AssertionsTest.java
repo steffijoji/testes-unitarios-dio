4f41b1a92b8e1d55e64dc54050d4be6c25781b01
Steffi <steffijoji@gmail.com>
2023-01-15 02:10:11 +0044

    exercícios e exemplos de testes unitários

--- src/test/java/one/digitalinnovation/junit/AssertionsTest.java
+++ src/test/java/one/digitalinnovation/junit/AssertionsTest.java
@@ -1,7 +1,10 @@
 package one.digitalinnovation.junit;
 
+// import org.junit.jupiter.api.Assertions.*; // evita de ter de digitar 'Assertions' todas as vezes
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 public class AssertionsTest {
 
@@ -12,5 +15,24 @@ public class AssertionsTest {
         int[] segundoLancamento = {10, 20, 30, 40, 60};
 
         Assertions.assertArrayEquals(primeiroLancamento, segundoLancamento);
+    }
+
+    @Test
+    void validarSeObjetoEstaNulo() {
+        Pessoa pessoa = null;
+
+        Assertions.assertNull(pessoa);
+
+        pessoa = new Pessoa("Luciano", LocalDate.now());
+
+        Assertions.assertNotNull(pessoa);
+    }
+
+    @Test
+    void validarNumerosDeTiposDiferentes() {
+        double valor = 5.0;
+        double outroValor = 5.0;
+
+        Assertions.assertEquals(valor, outroValor);
     }
 }
